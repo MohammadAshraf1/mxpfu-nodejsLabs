621364449fc998a6b6da6d75531b8a01b542a8e1
Mohammad Ashraf <mohammadashraf2055@gmail.com>
2025-02-25 16:39:27 -0500

    Describe the changes you made

--- routes/users.js
+++ routes/users.js
@@ -21,25 +21,37 @@ let users = [
         email:"joyalwhite@gamil.com",
         DOB:"21-03-1989",
     },
+    {
+        firstName:"Jon",
+        lastName:"Lovato",
+        email:"jonlovato@theworld.com",
+        DOB:"10/10/1995",
+    },
 ];
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    res.send(users);
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filtered_users = users.filter((user)=>user.email===email);
+  res.send(filtered_users);
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    users.push({
+        "firstName": req.query.firstName,
+        "lastName": req.query.lastName,
+        "email": req.query.email,
+        "DOB": req.query.DOB,
+    });
+    res.send("The user " + req.query.firstName + " has been added!");
+
 });
 
 
